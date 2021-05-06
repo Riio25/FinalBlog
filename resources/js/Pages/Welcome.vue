f55e5124522c0ef5b6fd7613e2f75dd61d1c56cf
Riio <superrio25@gmail.com>
2021-05-06 13:46:51 -0600

    First Commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="content">
+        <div class="header">
+            <h1>{{ msg }}</h1>
+            <p>What would you like to do?</p>
+            <div id="menu-options">
+                <a href="/write" data-ajax="false">Write</a> |
+                <a href="/read">Read</a>
+            </div>
+            <!--            <router-view />-->
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {},
+        data(){
+            return {
+                msg: "Welcome to Riio's Blog"
+            }
+        }
+    };
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .content {
+        margin-top: 60px;
+        text-align: center;
+    }
+    #menu-options {
+        color: #42b983;
+        margin: 20px;
+    }
+</style>
